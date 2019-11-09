f1ba136a01102b7c5524c9d06d56256d57975dd6
dattran01477 <dattran01477@gmail.com>
2019-11-09 10:48:09 +0700

    DAT: add socket borrowing

--- src/components/bookcart/BookCart.js
+++ src/components/bookcart/BookCart.js
@@ -1,25 +1,54 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import axios from 'axios'
 import React, { Component } from "react";
-import { Row, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { connect } from "react-redux";
 import { callApiAsPromise } from "../../api";
-import { actFetchBooks } from "../../data/actions/book";
-import { actFetchBooksCarousel } from "../../data/actions/book";
-import { ClapSpinner } from "react-spinners-kit";
 import {
-  Link,
-  useParams
-} from "react-router-dom";
-import { validate } from "@babel/types";
-import { AddTempCart, GetTempCart, fetchBooksObjectCart, resetBooksObjectCart, DELTempCartItem } from "../../data/actions/cart";
+  DELTempCartItem,
+  fetchBooksObjectCart,
+  resetBooksObjectCart
+} from "../../data/actions/cart";
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
+
+let stompClient = null;
+
+function onConnected() {
+  // Subscribe to the Public Topic
+  stompClient.subscribe("/topic/borrowing-list", onMessageReceived);
+}
+
+function onError(error) {
+  console.log("loi roi " + error);
+}
+
+function onMessageReceived(data) {
+  console.log(data);
+}
+
 export class BookCart extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: true,
-      bookCartLocal: []
+      bookCartLocal: [],
+      stompClient: null
     };
+  }
+
+  onclickSendData = data => {
+    this.state.stompClient.send(
+      "/apps/borrowing.add",
+      {},
+      JSON.stringify(data)
+    );
+  };
+
+  componentDidMount() {
+    var socket = new SockJS("http://localhost:8080/app/ws");
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, onConnected, onError);
+    this.setState({...this.state , stompClient:stompClient});
   }
 
   componentWillMount() {
@@ -27,9 +56,8 @@ export class BookCart extends Component {
     this.props.resetBooksObjectCart();
     bookCart.map(id => {
       this.getBookByCriteria(id);
-    })
+    });
   }
-
 
   initBookCartObject(id) {
     this.props.resetBooksObjectCart();
@@ -38,9 +66,9 @@ export class BookCart extends Component {
     console.log("Hello" + temp);
     temp.map(idBook => {
       this.getBookByCriteria(idBook);
-    })
+    });
   }
-  getBookByCriteria = (id) => {
+  getBookByCriteria = id => {
     let x = "books/" + id;
     callApiAsPromise("GET", x, null, null)
       .then(res => {
@@ -56,8 +84,7 @@ export class BookCart extends Component {
     e.preventDefault();
     this.props.delBooksCartById(id);
     this.initBookCartObject(id);
-
-  }
+  };
   handleSubmit = (user, bookId, editorId, e) => {
     bookId.map(id => {
       let body = {
@@ -65,37 +92,41 @@ export class BookCart extends Component {
         editorId: editorId,
         status: "waitting",
         userId: user
-      }
+      };
       const headers = {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json"
+      };
       console.log(body);
-      callApiAsPromise("POST", "borrowing-card/", null, JSON.stringify(body))
-        .then(res => {
-        })
-        .catch(err => {
-          alert(err);
-        });
-    })
-  }
+      this.onclickSendData(body);
+      // callApiAsPromise("POST", "borrowing-card/", null, JSON.stringify(body))
+      //   .then(res => {})
+      //   .catch(err => {
+      //     alert(err);
+      //   });
+    });
+  };
   render() {
     const { isLoading } = this.state;
     let bookCart = this.props.bookCartObject;
     let bookItems = [];
     bookCart.map(item => {
-      if (item.hasOwnProperty('id'))
+      if (item.hasOwnProperty("id"))
         bookItems.push(
           <tr className="row-table">
             <td>{item.name}</td>
             <td>{item.numberPages}</td>
-            <td><Button onClick={(e) => this.handleDelButton(item.id, e)}>Xóa</Button></td>
+            <td>
+              <Button onClick={e => this.handleDelButton(item.id, e)}>
+                Xóa
+              </Button>
+            </td>
           </tr>
-        )
-    })
+        );
+    });
     const divStyle = {
-      height: 'auto',
+      height: "auto"
     };
-    console.log(bookItems)
+    console.log(bookItems);
     return (
       <div>
         <h1>Thông tin mượn sách</h1>
@@ -110,13 +141,17 @@ export class BookCart extends Component {
                 <th>Xóa</th>
               </tr>
             </thead>
-            <tbody>
-              {bookItems}
-            </tbody>
+            <tbody>{bookItems}</tbody>
           </table>
         </div>
-        <Button onClick={(e) => { this.handleSubmit("Quan", this.props.bookCart, "Dat", e) }}>Xác nhận</Button>
-      </div >
+        <Button
+          onClick={e => {
+            this.handleSubmit("Quan", this.props.bookCart, "Dat", e);
+          }}
+        >
+          Xác nhận
+        </Button>
+      </div>
     );
   }
 }
@@ -128,21 +163,22 @@ const mapStateToProps = (state, ownProps) => {
   return {
     bookCart: state.tempCart,
     bookCartObject: state.tempCartObject
-  }
-}
+  };
+};
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    fetchBooksObjectCart: (data) => {
-      dispatch(fetchBooksObjectCart(data))
+    fetchBooksObjectCart: data => {
+      dispatch(fetchBooksObjectCart(data));
     },
     resetBooksObjectCart: () => {
-      dispatch(resetBooksObjectCart())
+      dispatch(resetBooksObjectCart());
     },
-    delBooksCartById: (data) => {
-      dispatch(DELTempCartItem(data))
+    delBooksCartById: data => {
+      dispatch(DELTempCartItem(data));
     }
-  }
-}
+  };
+};
 export default connect(
-  mapStateToProps, mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(BookCart);
